b29a0743c8b151427b8b475c2463f4201d633e56
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-04-27 22:55:22 +0430

    comments

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// strings can be added ogether with +
+
+	fmt.Println("go" + "lang")
+
+	// inegers and floats
+	
+	fmt.Println("1+1=", 7.0/3.0)
+
+
+// Go has various value types including strings,
+// integers, floats, booleans, etc. 
+
+
+// output :
+//golang
+//1+1 = 2
+//7.0/3.0 = 2.333333333333333
